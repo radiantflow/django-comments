6e948fdb5ba3f61a666fe10161dec359f26a25da
smoothify <dev@smoothify.com>
2014-08-16 12:31:39 +0100

    Remove post_comment view.

--- comments/views/comment.py
+++ comments/views/comment.py
@@ -42,6 +42,7 @@ def _lookup_content_object(data):
             parent_comment = None
             model = models.get_model(*ctype.split(".", 1))
             target = model._default_manager.get(pk=object_pk)
+            # model._default_manager.using(using).get(pk=object_pk)
         except TypeError:
             return CommentPostBadRequest(
                 "Invalid content_type value: %r" % escape(ctype))
@@ -53,125 +54,31 @@ def _lookup_content_object(data):
             return CommentPostBadRequest(
                 "No object matching content-type %r and object PK %r exists." % \
                     (escape(ctype), escape(object_pk)))
+        except (ValueError, ValidationError) as e:
+            return CommentPostBadRequest(
+                "Attempting go get content-type %r and object PK %r exists raised %s" % \
+                    (escape(ctype), escape(object_pk), e.__class__.__name__))
+
     else:
         return CommentPostBadRequest("Missing content_type or object_pk field.")
 
     return (target, parent_comment, model)
 
-@csrf_protect
-@require_POST
-def post_comment(request, next=None, using=None):
-    """
-    Post a comment.
-
-    HTTP POST is required. If ``POST['submit'] == "preview"`` or if there are
-    errors a preview template, ``comments/preview.html``, will be rendered.
-    """
-    # Fill out some initial data fields from an authenticated user, if present
-    data = request.POST.copy()
-    if request.user.is_authenticated():
-        if not data.get('user_name', ''):
-            data["user_name"] = request.user.get_full_name() or request.user.get_username()
-        if not data.get('user_email', ''):
-            data["user_email"] = request.user.email
-
-    # Look up the object we're trying to comment about
-    ctype = data.get("content_type")
-    object_pk = data.get("object_pk")
-    if ctype is None or object_pk is None:
-        return CommentPostBadRequest("Missing content_type or object_pk field.")
-    try:
-        model = models.get_model(*ctype.split(".", 1))
-        target = model._default_manager.using(using).get(pk=object_pk)
-    except TypeError:
-        return CommentPostBadRequest(
-            "Invalid content_type value: %r" % escape(ctype))
-    except AttributeError:
-        return CommentPostBadRequest(
-            "The given content-type %r does not resolve to a valid model." % \
-                escape(ctype))
-    except ObjectDoesNotExist:
-        return CommentPostBadRequest(
-            "No object matching content-type %r and object PK %r exists." % \
-                (escape(ctype), escape(object_pk)))
-    except (ValueError, ValidationError) as e:
-        return CommentPostBadRequest(
-            "Attempting go get content-type %r and object PK %r exists raised %s" % \
-                (escape(ctype), escape(object_pk), e.__class__.__name__))
-
-    # Do we want to preview the comment?
-    preview = "preview" in data
-
-    # Construct the comment form
-    form = comments.get_form()(target, data=data)
-
-    # Check security information
-    if form.security_errors():
-        return CommentPostBadRequest(
-            "The comment form failed security verification: %s" % \
-                escape(str(form.security_errors())))
-
-    # If there are errors or if we requested a preview show the comment
-    if form.errors or preview:
-        template_list = [
-            # These first two exist for purely historical reasons.
-            # Django v1.0 and v1.1 allowed the underscore format for
-            # preview templates, so we have to preserve that format.
-            "comments/%s_%s_preview.html" % (model._meta.app_label, model._meta.module_name),
-            "comments/%s_preview.html" % model._meta.app_label,
-            # Now the usual directory based template hierarchy.
-            "comments/%s/%s/preview.html" % (model._meta.app_label, model._meta.module_name),
-            "comments/%s/preview.html" % model._meta.app_label,
-            "comments/preview.html",
-        ]
-        return render_to_response(
-            template_list, {
-                "comment": form.data.get("comment", ""),
-                "form": form,
-                "next": data.get("next", next),
-            },
-            RequestContext(request, {})
-        )
-
-    # Otherwise create the comment
-    comment = form.get_comment_object()
-    comment.ip_address = request.META.get("REMOTE_ADDR", None)
-    if request.user.is_authenticated():
-        comment.user = request.user
-
-    # Signal that the comment is about to be saved
-    responses = signals.comment_will_be_posted.send(
-        sender=comment.__class__,
-        comment=comment,
-        request=request
-    )
-
-    for (receiver, response) in responses:
-        if response == False:
-            return CommentPostBadRequest(
-                "comment_will_be_posted receiver %r killed the comment" % receiver.__name__)
-
-    # Save the comment and signal that it was saved
-    comment.save()
-    signals.comment_was_posted.send(
-        sender=comment.__class__,
-        comment=comment,
-        request=request
-    )
-
-    return next_redirect(request, fallback=next or 'comments-comment-done',
-        c=comment._get_pk_val())
 
 comment_done = confirmation_view(
     template="comments/posted.html",
     doc="""Display a "comment was posted" success page."""
 )
 
-
+@csrf_protect
 def edit(request, comment_pk=None, parent_pk=None, ctype=None, object_pk=None, next=None, *args, **kwargs):
     """
     Edit or create a comment.
     Displays and processes the comment model form
+
+    If HTTP POST is present it processes and adds / edits comment.
+    If ``POST['submit'] == "preview"`` or if there are
+    errors a preview template, ``comments/preview.html``, will be rendered.
 
     """
 
@@ -237,6 +144,8 @@ def edit(request, comment_pk=None, parent_pk=None, ctype=None, object_pk=None, n
                  or request.user.has_perm("comments.can_moderate")):
                 return HttpResponse("Unauthorized", status=401)
 
+        # Do we want to preview the comment?
+        preview = "preview" in form_data
 
         if form.security_errors():
             # NOTE: security hash fails!
@@ -245,7 +154,7 @@ def edit(request, comment_pk=None, parent_pk=None, ctype=None, object_pk=None, n
                     escape(str(form.security_errors())))
 
         # If there are errors, or if a preview is requested
-        if form.errors or "preview" in form_data:
+        if form.errors or preview:
             app_label, model_name = (form.instance.content_type.app_label, form.instance.content_type.model)
             template_list = [
                 # These first two exist for purely historical reasons.
@@ -272,7 +181,8 @@ def edit(request, comment_pk=None, parent_pk=None, ctype=None, object_pk=None, n
         if form.is_valid():
             # Save the comment and signal that it was saved
             form.save()
-            return next_redirect(request, fallback= next or comment_done, c=comment.pk)
+            return next_redirect(request, fallback=next or comment_done, c=comment.pk)
+            #_get_pk_val()
         else:
             # If we got here, raise Bad Request error.
             return CommentPostBadRequest("Could not complete request!")
